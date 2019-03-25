60fb3ed70fb8ca0e007f4669f389b2d7e4827ec5
cooperq <cooperq@cooperq.com>
2019-03-25 16:38:08 -0700

    create flask interface for accepting files

--- src/postalinspector.py
+++ src/postalinspector.py
@@ -7,33 +7,36 @@ License: GPLv3
 import base64
 import email
 import hashlib
+import json
 import os
 import sys
 
 
 import requests
 from urlextract import URLExtract
+from flask import Flask, request, abort
+
+app = Flask(__name__)
+vtapi = os.getenv("VT_API_KEY")
+
+@app.route("/v1/message", methods=['POST'])
+def handle_message():
+    # extract eml from request
+    content = request.json
+    print(content)
+    if not "data" in content:
+        abort(400)
+
+    eml = email.message_from_bytes(base64.b64decode(content["data"]))
+
+    responses = parse_eml(eml)
+    return json.dumps(responses)
 
 
-def main():
+def parse_eml(eml):
     hashes = []
     urls = []
-    vtapi = os.getenv("VT_API_KEY")
-
-    if vtapi is None:
-        sys.exit("Virus total api key not found\nCreate an environment variable called VT_API_KEY")
-
-    try:
-        with open(sys.argv[1]) as f:
-            eml = email.message_from_file(f)
-    except (IndexError, FileNotFoundError) as e:
-        sys.exit("""
-        Postal Inspector - Inspect attachments and urls from an email in VT without uploading anything.
-
-        Usage:
-            postalinspector.py <path to eml file>
-        """)
-
+    responses = []
 
     for part in eml.walk():
         if part.get_content_disposition() != "attachment" and \
@@ -59,7 +62,7 @@ def main():
         response = requests.get('https://www.virustotal.com/vtapi/v2/file/report',
           params=params, headers=headers)
         json_response = response.json()
-        print(json_response)
+        responses.append(json_response)
 
     for url in urls:
         headers = {
@@ -69,7 +72,28 @@ def main():
         response = requests.post('https://www.virustotal.com/vtapi/v2/url/report',
           params=params, headers=headers)
         json_response = response.json()
-        print(json_response)
+        responses.append(json_response)
+
+    return responses
+
+
+def main():
+    if vtapi is None:
+        sys.exit("Virus total api key not found\nCreate an environment variable called VT_API_KEY")
+
+    try:
+        with open(sys.argv[1]) as f:
+            eml = email.message_from_file(f)
+    except (IndexError, FileNotFoundError) as e:
+        sys.exit("""
+        Postal Inspector - Inspect attachments and urls from an email in VT without uploading anything.
+
+        Usage:
+            postalinspector.py <path to eml file>
+        """)
+
+    responses = parse_eml(eml)
+    print(responses)
 
 if __name__ == "__main__":
     main()
